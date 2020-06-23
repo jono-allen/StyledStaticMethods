5e4365459fce4ffb6b3010a45b82ee343b94cd71
Jono Allen <jonothanallen@gmail.com>
2020-06-23 20:12:40 +1200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+import styled from "styled-components";
+
+const Part = ({ ...props }) => <Part.Element />;
+
+Part.Element = styled.div`
+  background-color: red;
+  height: 50px;
+  width: 50px;
+`;
+export default function App() {
+  return (
+    <div className="App">
+      <Part />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
